f2d03b8de960d17011ce0d09cd47a74472845744
FLAVIO MOURA ROCHA <your_fmrocha@gmail.com>
2017-08-23 16:33:34 -0700

    First commit

---
+++ WebFundamentals/Javascript/IDoDeclare/IDoDeclare.js
@@ -0,0 +1,19 @@
+var num1 = 34;
+var num2 = 46;
+var num3 = -84;
+var num4 = 10.1;
+var string1 = "This assignment ";
+var string2 = "is ";
+var string3 = "Amazing!";
+var go = true;
+var stop = false;
+var why;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(string1+string2+string3);
+console.log(go);
+console.log(stop);
+console.log(why);

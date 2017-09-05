a20148340e8b41813ab0a34bab717bd98a5e5872
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-09-05 15:05:06 -0700

    Upload Compare Lists assignment

---
+++ Python/CompareLists/CompareLists.py
@@ -0,0 +1,10 @@
+'''
+Compare lists assignment '''
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','milk']
+
+if list_one == list_two:
+  print("The lists are the same")
+else:
+  print("The lists are not the same")

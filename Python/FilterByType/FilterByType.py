fc91023bfaf2c5011acaece68481c7fecf03f64e
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-09-05 12:30:52 -0700

    Upload FilterByType assignment

---
+++ Python/FilterByType/FilterByType.py
@@ -0,0 +1,30 @@
+'''
+Filter by Type '''
+
+# Integer
+
+sI = 45
+
+if sI >= 100:
+    print("That's a big number!")
+else:
+    print("that's a small number!")
+
+# String
+
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+
+if len(bS) >= 50:
+    print("Long sentence.")
+else:
+    print("Short sentence.")
+
+
+# List
+
+aL = [1,7,4,21]
+
+if len(aL) >= 10:
+    print("Big list.")
+else:
+    print("Short list.")

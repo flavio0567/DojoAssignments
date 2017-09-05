afc7bc58e66b8fe1a24ea43991239533ae9349a2
FLAVIO MOURA ROCHA <fmrocha@gmail.com>
2017-09-05 15:27:01 -0700

    Upload Find Characters assignment

---
+++ Python/FindCharacters/FindCharacters.py
@@ -0,0 +1,12 @@
+'''
+Find Characters assignment '''
+
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+new_list = []
+
+for i in word_list:
+  if  char in i:
+    new_list.append(i)  
+
+print(new_list)
